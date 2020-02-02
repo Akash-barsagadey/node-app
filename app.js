bbc0dea0c562fb351465ea314509e70e9488ec7e
Akash Barsagadey <akash_udaram.barsagadey@syngenta.com>
2020-02-02 11:44:38 +0530

    added body-parser and POST request

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/add-user', (req, res, next) => {
+  res.send(
+    '<form action="/user" method="POST"><input type="text" name="title"><button type="submit">Add User</button></form>'
+  );
+});
+
 app.use('/user', (req, res, next) => {
-  console.log('/users middleware >>>>>>>>');
-  res.send('<H1>The Middleware that handles just users</H1>');
+  console.log(req.body);
+  res.redirect('/');
 });
 
 app.use('/', (req, res, next) => {
-  console.log('/ middleware');
-  res.send('<H2>The Middleware that handles just /</H2>');
+  res.send('<h1>Welcome from Express!</h1>');
 });
 
 app.listen(3000);
