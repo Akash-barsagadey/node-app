f038dcab783fe65a0402dbb04edfd7cd5d8084b6
Akash Barsagadey <akash_udaram.barsagadey@syngenta.com>
2020-02-02 11:32:37 +0530

    Start with express js

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-const http = require('http');
+const express = require('express');
 
-const routes = require('./routes');
+const app = express();
 
-console.log(routes.someText);
+app.use('/user', (req, res, next) => {
+  console.log('/users middleware >>>>>>>>');
+  res.send('<H1>The Middleware that handles just users</H1>');
+});
 
-const server = http.createServer(routes.handler);
+app.use('/', (req, res, next) => {
+  console.log('/ middleware');
+  res.send('<H2>The Middleware that handles just /</H2>');
+});
 
-server.listen(3000);
+app.listen(3000);
