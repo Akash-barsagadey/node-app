1b5a1e52cf923917f07ff50075ecc2455c49fb94
Akash Barsagadey <akash_udaram.barsagadey@syngenta.com>
2020-02-17 22:01:28 +0530

    Added model view controller

--- routes/admin.js
+++ routes/admin.js
@@ -2,14 +2,15 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../util/path');
+//As we are not using this now
+//const rootDir = require('../util/path');
+
+const productsController = require('../controllers/products');
 
 const router = express.Router();
 
-const products = [];
-
-// /admin/add-product => GET
-router.get('/add-product', (req, res, next) => {
+//As we create new controller
+/* router.get('/add-product', (req, res, next) => {
   //res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
   res.render('add-product', {
     pageTitle: 'Add Product',
@@ -18,13 +19,12 @@ router.get('/add-product', (req, res, next) => {
     productCSS: true,
     activeAddProduct: true
   });
-});
+}); */
+
+// /admin/add-product => GET
+router.get('/add-product', productsController.getAddProduct);
 
 // /admin/add-product => POST
-router.post('/add-product', (req, res, next) => {
-  products.push({ title: req.body.title });
-  res.redirect('/');
-});
+router.post('/add-product', productsController.postAddProduct);
 
-exports.routes = router;
-exports.products = products;
+module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -2,14 +2,13 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../util/path');
-const adminData = require('./admin');
+const productsController = require('../controllers/products');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  /* console.log('shop.js', adminData.products);
-  res.sendFile(path.join(rootDir, 'views', 'shop.html')); */
+/* router.get('/', (req, res, next) => {
+ // console.log('shop.js', adminData.products);
+ // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
   const products = adminData.products;
   res.render('shop', {
     prods: products,
@@ -19,6 +18,8 @@ router.get('/', (req, res, next) => {
     activeShop: true,
     productCSS: true
   });
-});
+}); */
+
+router.get('/', productsController.getProducts);
 
 module.exports = router;
